6146c4590bc653afca4a8c18130e1b2bb9b001b1
Shrey Bana <shreybana26@gmail.com>
2024-12-06 16:27:07 +0530

    Builder: Tests 1-5 & 9 passing.

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,51 +1,112 @@
 use proc_macro::TokenStream;
 use proc_macro2 as pm2;
 use quote::{format_ident, quote};
-use syn::{parse_macro_input, punctuated::{self, Punctuated}, Data::Struct, DeriveInput, Error, Field, Fields::Named, Result, Token};
+use syn::{
+    parse_macro_input, punctuated::Punctuated, Data::Struct, DeriveInput, Error, Field,
+    Fields::Named, Ident, Result, Token, Type,
+};
 
-fn into_builder_field(field: &Field) -> pm2::TokenStream {
-    let vis = field.vis.to_owned();
-    let id = format_ident!("b_{}", field.ident.to_owned().unwrap());
-    let t = field.ty.to_owned();
-    quote! {
-        #vis #id : Option<#t>
-    }.into()
+type PunctuatedTStream = Punctuated<pm2::TokenStream, Token![,]>;
+
+struct FieldMeta {
+    iden: Ident,
+    builder_iden: Ident,
+    typ: Type,
 }
 
-fn create_fields(derive: &DeriveInput) -> Result<Punctuated<pm2::TokenStream, Token![,]>> {
-    let fields = match derive.data {
-        Struct(ref ds) => match &ds.fields {
-            Named(fs) => Ok(fs.named.to_owned()),
-            _ => Err(Error::new_spanned(derive, "Un-named fields, cannot create builder."))
+fn field_meta(field: &Field) -> FieldMeta {
+    let fid = field.ident.as_ref().unwrap();
+    let bfid = format_ident!("b_{}", fid);
+    let t = field.ty.to_owned();
+    FieldMeta {
+        iden: fid.to_owned(),
+        builder_iden: bfid,
+        typ: t,
+    }
+}
+
+fn struct_fields(derive: &DeriveInput) -> Result<Vec<Field>> {
+    let fields = match &derive.data {
+        Struct(s) => match &s.fields {
+            Named(fs) => Ok(&fs.named),
+            _ => Err(Error::new_spanned(
+                derive,
+                "Un-named fields, cannot create builder.",
+            )),
         },
-        _ => Err(Error::new_spanned(derive, "Not a struct."))
+        _ => Err(Error::new_spanned(derive, "Not a struct.")),
     }?;
-    let i = fields.iter()
-        .map(into_builder_field);
-    Ok(Punctuated::from_iter(i))
+    Ok(fields.iter().map(|f| f.to_owned()).collect())
 }
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
+    let some = quote! { std::option::Option::Some };
+    let none = quote! { std::option::Option::None };
+    let option = quote! { std::option::Option };
     let input = parse_macro_input!(input as DeriveInput);
-    let fields = create_fields(&input).unwrap();
+    let fs = struct_fields(&input).unwrap();
+    let fmlist = fs.iter().map(field_meta).collect::<Vec<_>>();
+    let field_decl = fmlist
+        .iter()
+        .map(
+            |FieldMeta {
+                 builder_iden, typ, ..
+             }| quote! { #builder_iden : #option<#typ> },
+        )
+        .collect::<PunctuatedTStream>();
+    let builder_args = fmlist
+        .iter()
+        .map(|FieldMeta { builder_iden, .. }| quote! { #builder_iden : #none })
+        .collect::<PunctuatedTStream>();
+    let setters = fmlist
+        .iter()
+        .map(
+            |FieldMeta {
+                 iden,
+                 builder_iden,
+                 typ,
+             }| {
+                quote! {
+                    pub fn #iden(&mut self, arg: #typ) -> &mut Self {
+                        self.#builder_iden = #some(arg);
+                        self
+                    }
+                }
+            },
+        )
+        .collect::<pm2::TokenStream>();
+    let args = fmlist
+        .iter()
+        .map(
+            |FieldMeta {
+                 iden, builder_iden, ..
+             }| {
+                quote! { #iden : std::mem::take(&mut self.#builder_iden)? }
+            },
+        )
+        .collect::<PunctuatedTStream>();
+
     let id = input.ident;
     let bid = format_ident!("{}Builder", id);
-    let const_args: Punctuated<Option<pm2::TokenStream>, Token![,]> =
-                               Punctuated::from_iter(fields.iter().map(|_| None));
-
     let exprs = quote! {
-        struct #bid { #fields }
+        struct #bid { #field_decl }
+
+        impl #bid {
+            #setters
+
+            pub fn build(&mut self) -> #option<#id> {
+                #some(#id { #args })
+            }
+        }
 
         impl #id {
             pub fn builder() -> #bid {
-                #bid { #const_args }
+                #bid { #builder_args }
             }
         }
     };
 
     let t = TokenStream::from(exprs);
-    eprintln!("TOKENS: {}", t);
-
     t
 }
